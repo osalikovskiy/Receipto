1332627766bf5dc94bcebab5d2d2663411f21dcb
Oleh Salikovskyi <osalikovskyi@gmail.com>
2026-05-09 01:28:00 +0200

    chore: linting, formatting & CI setup

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/.expo/**',
+      '**/.turbo/**',
+      '**/metro.config.js',
+      '**/babel.config.js',
+      '**/tailwind.config.js',
+      '**/tailwind.config.ts',
+      '**/postcss.config.js',
+      '**/next.config.js',
+      '**/vitest.config.ts',
+    ],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+]
